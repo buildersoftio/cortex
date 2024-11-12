9796b8d49a64e001e4c3d21965ec0c1f7139d4cf
Enes Hoxha <eneshoxha@outlook.com>
2024-11-12 20:02:54 +0100

    [v1/feature/07]: Implement StateStore
    Add IStateStore
    Implement InMemoryStateStore
    Create Cortex.States project
    Add Group By Key Operator

---
+++ src/Cortex.States/Abstractions/IStateStore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Cortex.States
+{
+    public interface IStateStore<TKey, TValue>
+    {
+        TValue Get(TKey key);
+        void Put(TKey key, TValue value);
+        bool ContainsKey(TKey key);
+        void Remove(TKey key);
+        IEnumerable<KeyValuePair<TKey, TValue>> GetAll();
+    }
+}
---
+++ src/Cortex.Streams/Operators/GroupByKeyOperator.cs
@@ -0,0 +1,36 @@
+﻿using Cortex.States;
+using System;
+using System.Collections.Generic;
+
+namespace Cortex.Streams.Operators
+{
+    public class GroupByKeyOperator<TInput, TKey> : IOperator
+    {
+        private readonly Func<TInput, TKey> _keySelector;
+        private readonly IStateStore<TKey, List<TInput>> _stateStore;
+        private IOperator _nextOperator;
+
+        public GroupByKeyOperator(Func<TInput, TKey> keySelector, IStateStore<TKey, List<TInput>> stateStore)
+        {
+            _keySelector = keySelector;
+            _stateStore = stateStore;
+        }
+
+        public void Process(object input)
+        {
+            var typedInput = (TInput)input;
+            var key = _keySelector(typedInput);
+
+            var group = _stateStore.Get(key) ?? new List<TInput>();
+            group.Add(typedInput);
+            _stateStore.Put(key, group);
+
+            _nextOperator?.Process(new KeyValuePair<TKey, List<TInput>>(key, group));
+        }
+
+        public void SetNext(IOperator nextOperator)
+        {
+            _nextOperator = nextOperator;
+        }
+    }
+}
