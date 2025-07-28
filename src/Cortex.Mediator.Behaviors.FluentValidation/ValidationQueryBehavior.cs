11160dabf9e94e9161dbd934fc4e124b7b5f61e4
Enes Hoxha <eneshoxha@outlook.com>
2025-07-28 13:12:16 +0200

    v2/feature/[Add Validation Support for Cortex Mediator] #129:
    Add new library to enrich the functionality of Cortex.Mediator - Cortex.Mediator.Behaviors.FluentValidation
    
    Add ValidationCommandBehavior
    Add ValidationQueryBehavior
    
    Update README.md file

---
+++ src/Cortex.Mediator.Behaviors.FluentValidation/ValidationQueryBehavior.cs
@@ -0,0 +1,40 @@
+﻿using Cortex.Mediator.Queries;
+using FluentValidation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cortex.Mediator.Behaviors.FluentValidation
+{
+    public sealed class ValidationQueryBehavior<TQuery, TResult> : IQueryPipelineBehavior<TQuery, TResult>
+        where TQuery : IQuery<TResult>
+    {
+
+        private readonly IEnumerable<IValidator<TQuery>> _validators;
+
+
+        public async Task<TResult> Handle(TQuery query, QueryHandlerDelegate<TResult> next, CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TQuery>(query);
+            var failures = _validators
+                .Select(v => v.Validate(context))
+                .SelectMany(r => r.Errors)
+                .Where(f => f != null)
+                .ToList();
+
+            if (failures.Count() > 0)
+            {
+                var errors = failures
+                    .GroupBy(f => f.PropertyName)
+                    .ToDictionary(
+                        g => g.Key,
+                        g => g.Select(f => f.ErrorMessage).ToArray());
+
+                throw new Exceptions.ValidationException(errors);
+            }
+
+            return await next();
+        }
+    }
+}
